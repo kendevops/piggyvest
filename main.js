17a1c03be46b0bf97cb689c05e0d1ca5ee0eca9b
Kenneth Aladi <kennethmichael11@gmail.com>
2020-02-09 22:44:48 +0100

    started the event listerner for the submit element in the form

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+});
